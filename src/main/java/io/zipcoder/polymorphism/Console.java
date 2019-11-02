bd2bdf3dc940eda4febc6fd38be97260e3c4294c
kendrang <kendrang@users.noreply.github.com>
2019-11-02 10:31:41 -0400

    got console working

---
+++ src/main/java/io/zipcoder/polymorphism/Console.java
@@ -0,0 +1,29 @@
+package io.zipcoder.polymorphism;
+
+import java.util.Scanner;
+
+public class Console {
+
+
+    public static void print(String output, Object... args) {
+        System.out.printf(output, args);
+    }
+
+    public static void println(String output, Object... args) {
+        print(output + "\n", args);
+    }
+
+    public static String getStringInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+    public static Integer getIntegerInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        Integer userInput = scanner.nextInt();
+        return userInput;
+    }
+}
--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -4,4 +4,24 @@ package io.zipcoder.polymorphism;
  * Created by leon on 11/6/17.
  */
 public class MainApplication {
+    public static void main(String[] args) {
+
+        Boolean turnOn = true;
+
+        while (turnOn) {
+            Integer inputValue = Console.getIntegerInput("Hello nice to meet you! How many pets do you have?" +
+                    " \n Please enter a number value :)");
+
+            if (inputValue > 0) {
+                for (int i = 0; i < inputValue; i++) {
+                    String petValue = Console.getStringInput("What type of animal are they?");
+                    String nameValue = Console.getStringInput("What are their names?");
+                }
+
+            }
+            System.out.println("Thank you for telling me about your pets! ");
+            turnOn = false;
+
+        }
+    }
 }
